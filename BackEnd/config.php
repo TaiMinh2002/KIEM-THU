5d0480bb44f93509bf4ef281483e93824e6fb715
TaiMinh2002 <minhtaiphi@gmail.com>
2023-05-04 09:59:44 +0700

    kiem thu

---
+++ BackEnd/config.php
@@ -0,0 +1,38 @@
+<?php
+    date_default_timezone_set('Asia/Ho_Chi_Minh');
+    define('AUTO_DELETE_REPORT', '30'); // Delete report after 30 days
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'map');
+
+    define('SITE_NAME', 'Quản Lý Tuyến Đường');
+    define('SITE_ACRONYMS', 'QLTĐ');
+    define('SITE_URL', 'https://localhost/map/');
+
+    define('RUNNING_TEXT', 'Chào mừng bạn đến với trang quản lý tuyến đường');
+
+    define('CONTACT_NAME_ADMIN', 'Hoàng Khắc Phúc');
+    define('CONTACT_PHONE_ADMIN', '0563014144');
+    define('CONTACT_FB_NAME', 'Hoàng Khắc Phúc');
+    define('CONTACT_FB_URL', 'https://www.facebook.com/modcungken');    
+
+    
+    // define('DB_HOST', 'localhost');
+    // define('DB_USER', 'trungt10');
+    // define('DB_PASS', '55CpQ0U;u+okQ2');
+    // define('DB_NAME', 'trungt10_map');
+
+    // define('SITE_NAME', 'Quản Lý Tuyến Đường');
+    // define('SITE_ACRONYMS', 'QLTĐ');
+    // define('SITE_URL', 'https://trungtamdieukhienxapcth.net/');
+
+    // define('RUNNING_TEXT', 'CÔNG TY ĐIỆN LỰC THANH HOÁ - TRUNG TÂM ĐIỀU KHIỂN XA');
+
+    // define('CONTACT_NAME_ADMIN', 'Nguyễn Tuấn Anh');
+    // define('CONTACT_PHONE_ADMIN', '0912345678');
+    // define('CONTACT_FB_NAME', 'Nguyễn Tuấn Anh');
+    // define('CONTACT_FB_URL', 'https://www.facebook.com');    
+
+    
